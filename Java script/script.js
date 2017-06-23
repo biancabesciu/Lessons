a3c9ad1569923a2a885ef7adc43bcc06d69cddba
Bianca Besciu <bianca.besciu@gmail.com>
2017-06-23 12:21:00 +0200

    adding the Java script folder

---
+++ Java script/script.js
@@ -0,0 +1,18 @@
+let Pi = 3.14;
+
+function multiply() {
+    const M = document.getElementById('multiplier').value;
+    const answerBox = document.getElementById('answer');
+
+    answerBox.innerHTML = M * Pi;
+}
+
+
+
+let name = 'Bianca';
+
+function greeting (){
+    let answerBox = document.getElementById('greet');
+
+    answerBox.innerHTML = 'Hello' + name;
+}
