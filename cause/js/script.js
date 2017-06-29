1cd015b3a8b8ea2374de936dcb71e1b34fbeb833
Bianca Besciu <bianca.besciu@gmail.com>
2017-06-29 13:07:14 +0200

    made my first validation form with javaScript

---
+++ cause/js/script.js
@@ -0,0 +1,93 @@
+// Personal-details form validation ---------------------------------------->
+
+// Validating first name fields
+function validate(field) {
+
+    // set of rules for the input field
+    let re = /[A-Za-z -']$/;
+
+    if (re.test(document.getElementById(field).value)) {
+
+        // if true change the box background to green
+        document.getElementById(field).style.background = '#ccffcc';
+        return true;
+    } else {
+
+        // if false change the box background to red
+        document.getElementById(field).style.background = '#e35152';
+
+        //invalid first name
+        document.getElementById(field + 'Error').style.display = "block";
+        document.getElementById(field + 'Error').style.display = "none";
+        return false;
+    }
+}
+
+// Validate email
+function validateEmail(email){
+
+    // set of rules for the input field
+    let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if(re.test(email)){
+
+        // if true change the box background to green
+        document.getElementById('email').style.background ='#ccffcc';
+        document.getElementById('email-error').style.display = "none";
+        return true;
+    }else{
+
+        // if false change the box background to red
+        document.getElementById('email').style.background ='#e35152';
+        return false;
+    }
+}
+
+// Validate Selected Countries
+function validateSelect(x){
+
+    if(document.getElementById(x).selectedIndex !== 0){
+
+        // if true change the box background to green
+        document.getElementById(x).style.background ='#ccffcc';
+        document.getElementById(x + 'Error').style.display = "none";
+        return true;
+    }else{
+
+        // if false change the box background to red
+        document.getElementById(x).style.background ='#e35152';
+        return false;
+    }
+}
+
+
+//Validating entire form for valid data
+function validateForm() {
+
+    //Set error catcher
+    let error = 0;
+
+    // Check first name
+    if(!validate('firstName')){
+        document.getElementById('firstName-error').style.display = "block";
+        error++;
+    }
+
+    // Check  surname
+    if(!validate('surname')){
+        document.getElementById('surname-error').style.display = "block";
+        error++;
+    }
+
+
+    // Validate email
+    if(!validateEmail(document.getElementById('email').value)){
+        document.getElementById('email-error').style.display = "block";
+        error++;
+    }
+
+    // Don't submit form if there are errors
+    if(error > 0){
+        return false;
+    }
+}
