313280a4a6f9d2ea361dd755749bf43c0dd124c8
Bianca Besciu <bianca.besciu@gmail.com>
2017-08-03 16:56:05 +0200

    trying to make js work

--- FE101/BlackJackGame/script.js
+++ FE101/BlackJackGame/script.js
@@ -1,54 +1,63 @@
-let deck = ['2', '3', '4', '5', '6', '7', '8', '9',]
 
+let deck = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
+let resultElement = document.getElementById('result');
+let cardDisplay = document.getElementById('cards');
 
+// add event listener for btn
+//when click start function
+document.getElementById('deal').addEventListener('click', deal);
+document.getElementById('addCard').addEventListener('click', addCard);
+document.getElementById('score').addEventListener('click', score);
 
-
-
-
-//create deal function
 function deal() {
-    //declare new variable card,
-    //assign a random nr. between 1 and 52 (52 cards in a deck)
-     card = Math.floor(Math.random()*52+1);
+    let currentCards = document.getElementsByClassName('card');
 
-    //return random card
-    return card;
+
 }
 
-//deal first hand by,
-//declare new var card1 and card2,
-//assign value got by calling the function
-let card1 = deal();
-let card2 = deal();
+function addCard() {
+    let randomNumber = Math.floor(Math.random() * deck.length);
 
-//This function takes a card as a parameter and returns
-//the value of the card
-function dealCard(card) {
-    /*using modulus % - returns the remainder left after dividing
-     the left hand side with the right hand side*/
-    //if it's a face card, nr should be set to 10
-    if (card % 13 === 0 || card % 13 === 11 || card % 13 === 12 ) {
-        return 10;
+    checkGameStatus();
+    return deck[randomNumber];
+}
+
+//checking if the value is 21 or bigger
+function checkGameStatus() {
+    let currentCards = document.getElementsByClassName('card');
+
+    let totalValue = 0;
+
+    for (let i = 0; i < currentCards.length; i++) {
+        totalValue = totalValue + getCardValue( currentCards[ i ] );
     }
 
-    //if it's an ace
-    if (card % 13 === 1) {
-        return 11;
-    }
-
-    //otherwise return the remainder when card is divided by 13
-    else {
-        return card % 13;
+    if ( totalValue === 21 ) {
+        result.innerText = 'BLACKJACK!';
+    } else if ( totalValue > 21 ) {
+        result.innerText = 'You Lose!';
     }
 }
 
+//give value to the face and ace cards
+function getCardValue(cardElement) {
+    let cardValue = 0;
 
-//create score function
-//return score by adding up the cards passed through the getValue function
-function hit() {
-    
-    return startPlay(card1) + startPlay(card2);
+    if (cardElement.innerText === 'J' || cardElement.innerText === 'Q' || cardElement.innerText === 'K') {
+        cardValue = 10;
+
+    } else if (cardElement.innerText === 'A') {
+        cardValue = 11;
+
+    } else {
+        cardValue = Number(cardElement.innerText);
+
+    }
+
+    return cardValue;
 }
-console.log('You have cards ' + card1 + ' and ' + card2 + ' for a score of ' + hit());
 
+function score() {
+    resultElement.innerText = 'Your score is ' +;
+}
