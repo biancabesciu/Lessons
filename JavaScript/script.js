fff8b008ecb075deafa1c5e835e8044559dccfd5
Bianca Besciu <bianca.besciu@gmail.com>
2018-03-01 10:54:59 +0100

    javascript practice exercise

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,18 +1,48 @@
-let Pi = 3.14;
+let scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
 
-function multiply() {
-    const M = document.getElementById('multiplier').value;
-    const answerBox = document.getElementById('answer');
+let costs = [.25, .27, .25, .25, .25, .25, .33, .31, .25, .29, .27, .22, .31, .25, .25, .33, .21, .25, .25, .25, .28, .25, .24, .22, .20, .25, .30, .25, .24, .25, .25, .25, .27, .25, .26, .29];
 
-    answerBox.innerHTML = M * Pi;
+function printAndGetHighScore(scores) {
+    let highScore = 0;
+    let output;
+
+    for (let i = 0; i < scores.length; i++) {
+        output = "Bubble solution #" + i + " score: " + scores[i];
+        console.log(output);
+        if (scores[i] > highScore) {
+            highScore = scores[i];
+        }
+    }
+    return highScore;
 }
+let highScore = printAndGetHighScore(scores);
+console.log('Bubbles tests: ' + scores.length);
+console.log('Highest bubbles score: ' + highScore);
 
-
-
-let name = 'Bianca';
-
-function greeting (){
-    let answerBox = document.getElementById('greet');
-
-    answerBox.innerHTML = 'Hello ' + name;
+function getBestResults(scores, highScore) {
+    let solutionHighScore = [];
+    for (let i = 0; i < scores.length; i++) {
+        if (scores[i] === highScore) {
+            solutionHighScore.push(i)
+        }
+    }
+    return solutionHighScore;
 }
+let solutionHighScore = getBestResults(scores, highScore);
+console.log('Solutions with high score: ' + solutionHighScore);
+
+function getMostCostEffectiveSolution(scores, costs, highscore) {
+    let cost = 100;
+    let index;
+    for(let i = 0; i < scores.length; i++) {
+        if(scores[i] === highscore) {
+            if(cost > costs[i]) {
+                index = i;
+                cost = costs[i];
+            }
+        }
+    }
+    return index;
+}
+let mostCostEffective = getMostCostEffectiveSolution(scores, costs, highScore);
+console.log("Bubble Solution #" + mostCostEffective + " is the most cost effective");
